157997c75954ec2e8515d4505b6bd79491ecf489
Gary Magill <Garymagill22@gmail.com>
2023-07-10 11:39:36 -0600

    Fruity Loops

---
+++ src/main/java/com/gary/daikichifruityloops/FruityLoopsApplication.java
@@ -0,0 +1,19 @@
+package com.gary.daikichifruityloops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class FruityLoopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruityLoopsApplication.class, args);
+	}
+	
+	
+	
+
+}
